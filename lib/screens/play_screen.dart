c31e35ae3fe95ce7e1b0b0dccb611bde5f70dbb1
Jtility <leejewon97@gmail.com>
2024-06-26 00:38:11 +0900

    PlayScreen 레이아웃 일단 완료 GridView Expanded를 해제 나머지 위젯들은 Expanded(Center)로 감싸서 화면을 균등하게 나눔 결과적으로 GridView 고정 후, 나머지 가운데 정렬 나머지 위젯들의 Padding도 의미 없어져 삭제

--- lib/screens/play_screen.dart
+++ lib/screens/play_screen.dart
@@ -9,8 +9,7 @@ class PlayScreen extends StatelessWidget {
       backgroundColor: Colors.amber[100],
       body: Column(
         children: [
-          const Padding(
-            padding: EdgeInsets.symmetric(vertical: 40),
+          const Expanded(
             child: Center(
               child: Text(
                 '1',
@@ -18,48 +17,47 @@ class PlayScreen extends StatelessWidget {
               ),
             ),
           ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 5),
-              child: Center(
-                child: GridView.builder(
-                  shrinkWrap: true,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 5,
-                    mainAxisSpacing: 5,
-                    crossAxisSpacing: 5,
-                  ),
-                  itemCount: 25,
-                  itemBuilder: (context, index) {
-                    return TextButton(
-                      onPressed: () {},
-                      style: ButtonStyle(
-                        shape: MaterialStateProperty.all<OutlinedBorder?>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                            side: const BorderSide(
-                                color: Colors.black, width: 2.0),
-                          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5),
+            child: Center(
+              child: GridView.builder(
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 5,
+                  mainAxisSpacing: 5,
+                  crossAxisSpacing: 5,
+                ),
+                itemCount: 25,
+                itemBuilder: (context, index) {
+                  return TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all<OutlinedBorder?>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          side:
+                              const BorderSide(color: Colors.black, width: 2.0),
                         ),
                       ),
-                      child: Text(
-                        '${index + 1}',
-                        style: const TextStyle(fontSize: 40),
-                      ),
-                    );
-                  },
-                ),
+                    ),
+                    child: Text(
+                      '${index + 1}',
+                      style: const TextStyle(fontSize: 40),
+                    ),
+                  );
+                },
               ),
             ),
           ),
-          const Center(
-            child: Text(
-              '00 : 00',
-              style: TextStyle(fontSize: 60),
+          const Expanded(
+            child: Center(
+              child: Text(
+                '00 : 00',
+                style: TextStyle(fontSize: 60),
+              ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 40),
+          Expanded(
             child: Center(
               child: IconButton(
                 onPressed: () {},
