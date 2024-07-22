4a5d6a664ebdb93340caa88a60d53cd39fd56946
Jtility <leejewon97@gmail.com>
2024-07-23 00:57:04 +0900

    RecordScreen 리팩토링 및 폰트 크기 반응형 적용 record_screen.dart   RecordStyle 클래스 생성     Text들의 공통된 Style을 따로 빼기 위함   Row를 LayoutBuilder로 감쌈     RecordStyle에 index와 constraints을 넘겨줌       인스턴스를 변수로 만들어 Text들의 style로 사용

--- lib/screens/record_screen.dart
+++ lib/screens/record_screen.dart
@@ -38,40 +38,30 @@ class RecordScreen extends StatelessWidget {
                   itemBuilder: (context, index) {
                     return ListTile(
                       title: Center(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Expanded(
-                              child: Text(
-                                "${index == 0 ? '1st' : index == 1 ? '2nd' : index == 2 ? '3rd' : '${index + 1}th'}${recordTimes[index].contains('\t') ? '\t${recordTimes[index].split('\t')[0]}' : ''}",
-                                style: TextStyle(
-                                  fontSize: 30,
-                                  color: index == 0
-                                      ? Colors.indigoAccent
-                                      : index == 1
-                                          ? Colors.indigo
-                                          : index == 2
-                                              ? Colors.indigo[900]
-                                              : Colors.black,
+                        child: LayoutBuilder(
+                          builder: (context, constraints) {
+                            final recordStyle = RecordStyle(
+                              recordIndex: index,
+                              recordSize: constraints.maxWidth / 12,
+                            );
+                            return Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Expanded(
+                                  child: Text(
+                                    "${index == 0 ? '1st' : index == 1 ? '2nd' : index == 2 ? '3rd' : '${index + 1}th'}${recordTimes[index].contains('\t') ? '\t${recordTimes[index].split('\t')[0]}' : ''}",
+                                    style: recordStyle,
+                                  ),
                                 ),
-                              ),
-                            ),
-                            Text(
-                              recordTimes[index].contains('\t')
-                                  ? recordTimes[index].split('\t')[1]
-                                  : recordTimes[index],
-                              style: TextStyle(
-                                fontSize: 30,
-                                color: index == 0
-                                    ? Colors.indigoAccent
-                                    : index == 1
-                                        ? Colors.indigo
-                                        : index == 2
-                                            ? Colors.indigo[900]
-                                            : Colors.black,
-                              ),
-                            ),
-                          ],
+                                Text(
+                                  recordTimes[index].contains('\t')
+                                      ? recordTimes[index].split('\t')[1]
+                                      : recordTimes[index],
+                                  style: recordStyle,
+                                ),
+                              ],
+                            );
+                          },
                         ),
                       ),
                     );
@@ -90,4 +80,20 @@ class RecordScreen extends StatelessWidget {
       ),
     );
   }
+}
+
+class RecordStyle extends TextStyle {
+  RecordStyle({
+    required int recordIndex,
+    required double recordSize,
+  }) : super(
+          fontSize: recordSize,
+          color: recordIndex == 0
+              ? Colors.indigoAccent
+              : recordIndex == 1
+                  ? Colors.indigo
+                  : recordIndex == 2
+                      ? Colors.indigo[900]
+                      : Colors.black,
+        );
 }
